9d1bb2b1d40c90360f9351fb24a68c37c365f7b2
Neheniah Owens <nehemiahowens@outlook.com>
2016-07-08 21:40:05 -0600

    Cities added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709010136) do
+ActiveRecord::Schema.define(version: 20160709033531) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "population"
+    t.string   "elevation"
+    t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "first_name"
